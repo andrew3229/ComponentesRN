bdf8577e276b143f23c13def3f62c6b1d3bb6acd
Andres Camacho <dacv3229@gmail.com>
2024-03-31 12:24:36 -0500

    estructura general de proyectos RN

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Text, View, StyleSheet } from 'react-native'
+export const App = () => {
+  return (
+    <View>
+      <Text> App</Text>
+    </View>
+  )
+}
---
+++ src/screens/home/HomeScreens.tsx
@@ -0,0 +1,8 @@
+import { Text, View, StyleSheet } from 'react-native'
+export const HomeScreens = () => {
+    return (
+        <View>
+            <Text> Home Screens</Text>
+        </View>
+    )
+}
